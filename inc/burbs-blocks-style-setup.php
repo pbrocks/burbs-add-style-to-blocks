351501f59f4a22c1aebdd6c1ecdb9293c58c24bc
Paul Barthmaier <paul@barthmaier.rocks>
2019-12-02 00:22:52 -0500

    assets

--- inc/burbs-blocks-style-setup.php
+++ inc/burbs-blocks-style-setup.php
@@ -1,28 +1,14 @@
 <?php
 
-add_action( 'enqueue_block_assets', 'enqueue_burbs_blocks_style_scripts_styles' );
-/**
- * Register style and script.
- *
- * @since 0.1.1
- */
-function enqueue_burbs_blocks_style_scripts_styles() {
-	wp_register_style( 'burbs-blocks-style', plugins_url( 'css/burbs-blocks-style.css', __FILE__ ), [], time() );
+add_action( 'enqueue_block_assets', 'enqueue_burbs_block_script' );
+function enqueue_burbs_block_script() {
 	wp_register_script( 'burbs-blocks-style', plugins_url( 'js/burbs-blocks-style.js', __FILE__ ), [], time(), true );
+	wp_enqueue_script( 'burbs-blocks-style' );
 }
 
-
-add_filter( 'render_block', 'burbs_blocks_style_show_block_type', 10, 2 );
-
-/**
- * Restrict enqueueing to paragraph block.
- *
- * @since 0.1.1
- */
-function burbs_blocks_style_show_block_type( $block_content, $block ) {
-	if ( 'core/paragraph' === $block['blockName'] ) {
-		wp_enqueue_style( 'burbs-blocks-style' );
-		wp_enqueue_script( 'burbs-blocks-style' );
-	}
-	return $block_content;
+add_action( 'wp_enqueue_scripts', 'enqueue_burbs_block_style' );
+function enqueue_burbs_block_style() {
+	wp_register_style( 'burbs-blocks-style', plugins_url( 'css/burbs-blocks-style.css', __FILE__ ), [], time() );
+	wp_enqueue_style( 'burbs-blocks-style' );
 }
+
